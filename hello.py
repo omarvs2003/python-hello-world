725ed42e4cee14bbde7d58beee8305c2f24bb4d5
omarvs2003 <153531264+omarvs2003@users.noreply.github.com>
2025-04-05 17:16:13 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -15,7 +15,7 @@ class HelloHandler(BaseHTTPRequestHandler):
                 <h1>¡Hola mundo desde Github! Ahora estoy conectado a Jenkins :) gatito!!!! perrito!!!!</h1>
                 <p>Ahora vengo de un hook</p>
                 <p>PD. Omar V, Edgar R, Ruber T</p>
-                <p> test4 desde github </p>
+                <p> test5 desde github </p>
                 
             </body>
         </html>
