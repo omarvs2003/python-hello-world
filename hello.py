2f1632484eedcb11134f156d2d74cb8f8abd56c6
omarvs2003 <153531264+omarvs2003@users.noreply.github.com>
2025-04-05 13:45:22 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,31 @@
-print("¡Hola mundo desde Github! Ahora estoy conectado a Jenkins :) gatito!!!! perrito!!!! ")
-print("Ahora vengo de un hook")
-print("Conejito!!!")
-print("test2")
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class HelloHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Enviar respuesta HTTP 200 (OK)
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        
+        # Mensaje que quieres mostrar en el navegador
+        message = """
+        <html>
+            <head><title>Hola desde Docker</title></head>
+            <body>
+                <h1>¡Hola mundo desde Github! Ahora estoy conectado a Jenkins :) gatito!!!! perrito!!!!</h1>
+                <p>Ahora vengo de un hook</p>
+                <p>Conejito!!!</p>
+                <p>test2</p>
+            </body>
+        </html>
+        """
+        
+        # Escribir la respuesta al navegador
+        self.wfile.write(message.encode('utf-8'))
+
+if __name__ == "__main__":
+    # Configurar el servidor para que escuche en todas las interfaces (0.0.0.0) y en el puerto 8090
+    server_address = ('0.0.0.0', 8090)
+    httpd = HTTPServer(server_address, HelloHandler)
+    print("Servidor corriendo en el puerto 8090...")
+    httpd.serve_forever()
